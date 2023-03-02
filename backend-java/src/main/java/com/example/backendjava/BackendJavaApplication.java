9a9e2b08c5d4ac770a217f09bfbbefd92d8024ad
unclechuen <johnny@johnnyip.com>
2023-03-02 17:03:13 +0800

    init

---
+++ backend-java/src/main/java/com/example/backendjava/BackendJavaApplication.java
@@ -0,0 +1,18 @@
+package com.example.backendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class})
+public class BackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaApplication.class, args);
+    }
+
+}
