e81e78cb5e8c5cc808ed658684b85486307b691c
unclechuen <johnny@johnnyip.com>
2023-04-03 04:03:46 +0800

    Create CORSConfig.java

---
+++ backend-java/src/main/java/com/example/backendjava/security/CORSConfig.java
@@ -0,0 +1,19 @@
+package com.example.backendjava.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Change this to the specific origin you want to allow, or use "*" to allow all
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
