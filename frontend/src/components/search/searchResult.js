7fa7b534a131d55c2d742cda072fb1049dba16a4
unclechuen <johnny@johnnyip.com>
2023-04-17 00:52:15 +0800

    Result Pagination

--- frontend/src/components/search/searchResult.js
+++ frontend/src/components/search/searchResult.js
@@ -5,8 +5,23 @@ import ResultItem from './resultItem'
 
 const SearchResult = (props) => {
     let result = props.result
+    const itemPerPage = 5;
 
     const [activePage, setPage] = useState(1);
+    const [resultInPage, setResultInPage] = useState([])
+
+    useEffect(() => {
+        if (Object.keys(result).length !== 0) {
+            let start = (activePage - 1) * itemPerPage;
+            let end = start + itemPerPage;
+            let dataInPage = result.data.result.slice(start, end)
+            setResultInPage(dataInPage)
+            // console.log(dataInPage)
+
+        }
+
+    }, [result, activePage])
+
 
     if (Object.keys(result).length !== 0) {
         return (
@@ -20,7 +35,12 @@ const SearchResult = (props) => {
                         marginTop: "10px"
                     }}>
 
-                    {[...result.data.result].map((item, index) => {
+                    {/* {[...result.data.result].map((item, index) => {
+                        return (
+                            <ResultItem item={item} key={index} />
+                        )
+                    })} */}
+                    {[...resultInPage].map((item, index) => {
                         return (
                             <ResultItem item={item} key={index} />
                         )
